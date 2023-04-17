2b07ec2725288e5b85db41c80b837f6b54f129a5
cleverapollo <cleverapollo@gmail.com>
2023-04-17 19:05:50 +0700

    Implement farmer entity screen

--- lib/ui/screens/perform/farmer_member/entity/entity_farmer_screen.dart
+++ lib/ui/screens/perform/farmer_member/entity/entity_farmer_screen.dart
@@ -11,81 +11,76 @@ import 'package:cmo/ui/widget/cmo_option_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class EntityFarmerScreen extends StatefulWidget {
-  const EntityFarmerScreen({super.key});
+import 'package:cmo/extensions/iterable_extensions.dart';
+import 'package:cmo/l10n/l10n.dart';
+import 'package:cmo/ui/components/entity_list.dart';
+import 'package:cmo/ui/widget/cmo_app_bar.dart';
+import 'package:cmo/ui/widget/cmo_buttons.dart';
+import 'package:flutter/material.dart';
 
-  static void push(BuildContext context) {
-    Navigator.of(context).pushReplacement(
+class EntityFarmerScreen<T> extends StatefulWidget {
+  final T? selectedEntity;
+
+  const EntityFarmerScreen({this.selectedEntity, super.key});
+
+  static Future push<T>(BuildContext context, {T? selectedItem}) {
+    return Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (_) => const EntityFarmerScreen(),
+        builder: (_) => EntityFarmerScreen(selectedEntity: selectedItem),
       ),
     );
   }
 
   @override
-  State<EntityFarmerScreen> createState() => FfarmerEntityScreenState();
+  State<EntityFarmerScreen> createState() =>
+      _EntityFarmerScreenState();
 }
 
-class FfarmerEntityScreenState extends State<EntityFarmerScreen> {
-  Entity? selected;
+class _EntityFarmerScreenState<T> extends State<EntityFarmerScreen<T>> {
+  var entities = [
+    EntityItem("Farmer entity 1", displayString: () => "Farmer entity 1"),
+    EntityItem("Farmer entity 2", displayString: () => "Farmer entity 2"),
+    EntityItem("Farmer entity 3", displayString: () => "Farmer entity 3"),
+  ];
+  T? _selectedItem;
 
-  Future<void> toEntitySearchScreen(EntityType type) async {
-    final result =
-        await EntitySearchScreen.push(context, type: type, selected: selected);
-    if (result is! Entity) return;
-
-    selected = result;
-    setState(() {});
-  }
-
-  Widget buildOptionType({required EntityType type, required String title}) {
-    return CmoTappable(
-      onTap: () => toEntitySearchScreen(type),
-      child: CmoOptionTile(title: title),
-    );
-  }
-
-  Future<void> submit() async {
-    if (selected == null) return;
-    await context.read<UserDeviceCubit>().createUserDevice(context);
-    if (context.mounted) {
-      await context.read<EntityCubit>().sync(selected!);
-    }
-    if (context.mounted) CmoDashboardBase.push(context);
+  @override
+  void initState() {
+    super.initState();
+    _selectedItem = widget.selectedEntity;
   }
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        return false;
-      },
+    return CmoTappable(
+      onTap: FocusScope.of(context).unfocus,
       child: Scaffold(
-        appBar: CmoAppBar(title: LocaleKeys.entity.tr()),
-        resizeToAvoidBottomInset: false,
+        appBar: CmoAppBar(
+          title: LocaleKeys.entity.tr(),
+        ),
         body: Column(
           children: [
-            CmoHeaderTile(title: LocaleKeys.groupSchemeManagement.tr()),
-            buildOptionType(
-              type: EntityType.gs,
-              title: LocaleKeys.groupScheme.tr(),
+            Expanded(
+              child: EntityList(
+                // TODO: Should fetch them from db instead of hard-coded
+                entityItems: entities,
+                selectedItem:
+                entities.firstWhereOrNull((e) => e.rawData == _selectedItem),
+                onTap: (item) {
+                  _selectedItem = item.rawData as T?;
+                  setState(() {});
+                },
+              ),
             ),
-            buildOptionType(
-              type: EntityType.rmu,
-              title: LocaleKeys.resourceManagementUnit.tr(),
-            ),
-            const SizedBox(height: 51),
-            CmoHeaderTile(title: LocaleKeys.operationsManagement.tr()),
-            buildOptionType(
-              type: EntityType.cpy,
-              title: LocaleKeys.company.tr(),
-            ),
-            const Spacer(),
             Padding(
-              padding: const EdgeInsets.only(bottom: 40),
+              padding: const EdgeInsets.only(top: 40, bottom: 40),
               child: CmoFilledButton(
                 title: LocaleKeys.sync.tr(),
-                onTap: () => submit(),
+                onTap: _selectedItem == null
+                    ? null
+                    : () {
+                  print("Sync data");
+                },
               ),
             ),
           ],
@@ -94,3 +89,4 @@ class FfarmerEntityScreenState extends State<EntityFarmerScreen> {
     );
   }
 }
+
