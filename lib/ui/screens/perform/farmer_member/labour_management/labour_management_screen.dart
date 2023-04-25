27cd08fc56e7456f4a0b8d0bd9e23c8b28aa6527
cleverapollo <cleverapollo@gmail.com>
2023-04-25 19:08:11 +0700

    create UI for labour management screen

---
+++ lib/ui/screens/perform/farmer_member/labour_management/labour_management_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LabourManagementScreen extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => _LabourManagementScreenState();
+}
+
+class _LabourManagementScreenState extends State<LabourManagementScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
