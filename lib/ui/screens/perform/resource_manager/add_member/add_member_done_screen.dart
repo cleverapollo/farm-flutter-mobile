e2096a7c6838b26b50aadb23204b1ca1fd4b39c4
cleverapollo <cleverapollo@gmail.com>
2023-09-08 14:14:39 +0700

    Bug/AR 392 - fix header content signed contract for done screen

--- lib/ui/screens/perform/resource_manager/add_member/add_member_done_screen.dart
+++ lib/ui/screens/perform/resource_manager/add_member/add_member_done_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cmo/l10n/l10n.dart';
 import 'package:cmo/model/data/farm.dart';
 import 'package:cmo/state/add_member_cubit/add_member_cubit.dart';
+import 'package:cmo/state/add_member_cubit/add_member_state.dart';
 import 'package:cmo/state/dashboard/dashboard_cubit.dart';
 import 'package:cmo/ui/screens/cmo_dashboard_base.dart';
 import 'package:cmo/ui/theme/app_theme.dart';
@@ -11,6 +12,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AddMemberDone extends StatelessWidget {
   const AddMemberDone({super.key, this.farm});
+
   final Farm? farm;
 
   static Future<void> push(BuildContext context, {Farm? farm}) {
@@ -20,42 +22,50 @@ class AddMemberDone extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: CmoAppBarV2(
-          title: LocaleKeys.addMember.tr(),
-          subtitle: farm?.farmName ?? '',
-        ),
-        body: SizedBox.expand(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 12.0),
-            child: Column(
-              children: [
-                Text(
-                    '${farm?.firstName ?? ''} ${farm?.lastName ?? ''} ${LocaleKeys.member_is_now_a_group_scheme_member.tr()}',
-                    style: context.textStyles.bodyNormal
-                        .copyWith(color: context.colors.black)),
-                Text(
-                    LocaleKeys
-                        .documentation_has_been_emailed_to_the_relevant_parties
-                        .tr(),
-                    style: context.textStyles.bodyNormal
-                        .copyWith(color: context.colors.black)),
-                const Spacer(),
-                Center(
-                    child: CmoFilledButton(
-                        title: LocaleKeys.close.tr(),
-                        onTap: () {
-                          context.read<AddMemberCubit>().onDataChangeClose();
-                          context.read<AddMemberCubit>().stepCount();
-                          context
-                              .read<DashboardCubit>()
-                              .getResourceManagerMembers();
-                          CmoDashboardBase.push(context);
-                        })),
-                const SizedBox(height: 20),
-              ],
+    return BlocSelector<AddMemberCubit, AddMemberState, Farm?>(
+      selector: (state) => state.farm,
+      builder: (context, farmState) {
+        return Scaffold(
+            appBar: CmoAppBarV2(
+              title: LocaleKeys.addMember.tr(),
+              subtitle: farmState?.farmName ?? '',
             ),
-          ),
-        ));
+            body: SizedBox.expand(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 12.0),
+                child: Column(
+                  children: [
+                    Text(
+                        '${farmState?.firstName ?? farm?.firstName ?? ''} ${farmState?.lastName ?? farm?.lastName ?? ''} ${LocaleKeys.member_is_now_a_group_scheme_member.tr()}'
+                            .trimLeft(),
+                        style: context.textStyles.bodyNormal
+                            .copyWith(color: context.colors.black)),
+                    Text(
+                        LocaleKeys
+                            .documentation_has_been_emailed_to_the_relevant_parties
+                            .tr(),
+                        style: context.textStyles.bodyNormal
+                            .copyWith(color: context.colors.black)),
+                    const Spacer(),
+                    Center(
+                        child: CmoFilledButton(
+                            title: LocaleKeys.close.tr(),
+                            onTap: () {
+                              context
+                                  .read<AddMemberCubit>()
+                                  .onDataChangeClose();
+                              context.read<AddMemberCubit>().stepCount();
+                              context
+                                  .read<DashboardCubit>()
+                                  .getResourceManagerMembers();
+                              CmoDashboardBase.push(context);
+                            })),
+                    const SizedBox(height: 20),
+                  ],
+                ),
+              ),
+            ));
+      },
+    );
   }
 }
