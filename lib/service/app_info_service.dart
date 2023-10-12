cfc8842442a61b5fcddd6b36cb1d4a3e83bf4372
cleverapollo <cleverapollo@gmail.com>
2023-10-12 14:59:38 +0700

    bump version for release 1.0.3

--- lib/service/app_info_service.dart
+++ lib/service/app_info_service.dart
@@ -4,7 +4,7 @@ import 'package:cmo/model/app_mode.dart';
 export '../model/app_mode.dart';
 
 class AppInfoService {
-  String get version => '1.0.2';
+  String get version => '1.0.3';
   String get appName => 'Empower';
   String get databaseName => 'cmo.db';
   int get sqfliteImportPageSize => 100;
